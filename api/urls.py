a90b82e17afe1f2a7e6b716778a3cc02ccceed7b
goutambseervi <goutam@mirrorloops.com>
2020-02-23 06:24:58 +0530

    ocr implementation

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path, reverse_lazy
 from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 
+from api import settings
 from api.fileupload.views import FileUploadView
 from api.users.views import UserViewSet, CustomAuthToken
 
@@ -17,3 +19,5 @@ urlpatterns = [
     path('api/v1/upload/', FileUploadView.as_view()),
     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
